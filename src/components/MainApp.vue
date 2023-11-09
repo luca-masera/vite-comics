e0436d3d808b45b6c65a519b147915c190a6be5b
luca-masera <lucamasera1995@gmail.com>
2023-11-09 17:36:43 +0100

    main

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -3,8 +3,13 @@
         <div class="height-black">
             <h2>Content goes here </h2>
         </div>
-        <div>
+        <div class="height-blu">
             <ul>
+                <li v-for="(card, index) in listCard">
+
+                    {{ card.text.img }}
+
+                </li>
 
             </ul>
 
@@ -14,14 +19,54 @@
 
 <script>
 export default {
-    name: 'MainApp'
+    name: 'MainApp',
+    data() {
+
+        return {
+            listCard: [
+
+                {
+                    img: '/images/buy-comics-digital-comics',
+                    text: 'DIGITAL COMICS'
+                },
+                {
+                    img: '/images/buy-comics-mercandise',
+                    text: 'DC MERCANDISE'
+                },
+                {
+                    img: '/images/buy-comics-digital-comics',
+                    text: 'DIGITAL COMICS'
+                },
+                {
+                    img: '/images/buy-comics-shop-locator',
+                    text: 'SHO LOCATOR'
+                }, {
+                    img: '/images/buy-comics-subscriptions',
+                    text: 'SUBSCRIPTIONS'
+                },
+            ]
+
+
+        }
+    }
+
 }
 </script>
 
 <style lang="css" scoped>
+main {
+    height: 300px;
+    background-color: blue;
+}
+
 .height-black {
     height: 150px;
     background-color: black;
+}
+
+.height-blu {
+    height: 150px;
+    background-color: blue;
 }
 
 h2 {
