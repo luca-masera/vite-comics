4e704f7a7fd4a9d95c9009c7a4004825b1cb0dba
luca-masera <lucamasera1995@gmail.com>
2023-11-09 18:21:06 +0100

    footer

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -1,7 +1,60 @@
 <template>
-    <div>
-        <h3>Footer</h3>
-    </div>
+    <footer class="heigth padding-left ">
+        <div class="d-flex  ">
+            <div>
+                <ul class="pt-4">
+                    <li>
+                        <h5>DC COMICS</h5>
+                    </li>
+                    <li>TV</li>
+                    <li>TV</li>
+                    <li>TV</li>
+                    <li>TV</li>
+                    <li>TV</li>
+                    <li>TV</li>
+                    <li>TV</li>
+                    <li>
+                        <h5>SHOP</h5>
+                    </li>
+                    <li>shop</li>
+                    <li>shop</li>
+
+                </ul>
+            </div>
+            <div>
+                <ul class="pt-4">
+                    <li>
+                        <h5>DC COMICS</h5>
+                    </li>
+                    <li>TV</li>
+                    <li>TV</li>
+                    <li>TV</li>
+                    <li>TV</li>
+                    <li>TV</li>
+                    <li>TV</li>
+                    <li>TV</li>
+                    <li>TV</li>
+                    <li>shop</li>
+                    <li>shop</li>
+
+                </ul>
+            </div>
+            <ul class="pt-4">
+                <li>
+                    <h5>DC COMICS</h5>
+                </li>
+                <li>TV</li>
+                <li>TV</li>
+                <li>TV</li>
+
+            </ul>
+
+
+            <div class="d-flex justify-content-end">
+                <img :src="'/images/dc-logo-bg.png'" alt="">
+            </div>
+        </div>
+    </footer>
 </template>
 
 <script>
@@ -10,4 +63,26 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="css" scoped>
+.heigth {
+    height: 300px;
+    background-image: url('/images/footer-bg.jpg')
+}
+
+
+img {
+    width: 20%;
+}
+
+
+.padding-left {
+    padding-left: 300px;
+}
+
+li {
+
+    color: white;
+    font-size: 12px;
+    list-style: none;
+}
+</style>
